5225501ae2b94d0e6eea01f19776dfa01050fa79
darshak3112 <darshakkakadiya3112@gmail.com>
2025-07-12 12:35:51 +0530

    fix: adjust overflow behavior and make navbar sticky

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg border-b border-gray-200">
+    <nav className="bg-white sticky top-0 shadow-lg border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo */}
