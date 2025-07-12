81341ab5f0e765297d3e74ddc04e912cf829db1b
darshak <darshakkakadiya@fynd-external.com>
2025-07-12 09:55:44 +0530

    config : tailwind css added

--- client/src/components/sample-comp.tsx
+++ client/src/components/sample-comp.tsx
@@ -3,8 +3,8 @@ import { sampleUtil } from '../utils/sample-util'
 
 const SampleComp = () => {
   return (
-    <div>
-        <h1>Sample Comp</h1>
+    <div className='bg-gray-100 p-4 rounded-lg shadow-md h-full'>
+        <h1 className='text-2xl text-red-500'>Sample Comp</h1>
         <p>{sampleUtil()}</p>
     </div>
   )
