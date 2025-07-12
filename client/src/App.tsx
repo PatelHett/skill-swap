026d8346bd3da38bf757f6bfc665b1fa63237ccd
smit <smitdhimar61@gmail.com>
2025-07-12 11:11:57 +0530

    feat:add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
 // src/App.tsx
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-import AuthExample from './components/AuthExample';
-
+import Navbar from './components/Navbar';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import SamplePage from './pages/sample-page';
 
 function App() {
   return (
     <Provider store={store}>
-      <AuthExample />
+      <BrowserRouter> 
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <main className="pt-16">
+            <Routes>
+              <Route path="/" element={<SamplePage />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 }
