fd4843cef6298e5d76ee1a966b5ebda59b42a7df
shivvang <shivanghariakar@gmail.com>
2025-07-12 14:17:09 +0530

    profile visibiliy ,and couple of changes

--- backend/src/routes/auth.router.js
+++ backend/src/routes/auth.router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { auth } from "../controllers/auth.controller.js";
+import { verifyAccessToken } from "../middleware/middleware.js";
 const authRouter = express.Router();
 
 authRouter.get("/check-username", auth.checkUsername);
@@ -7,8 +8,14 @@ authRouter.post("/register", auth.registerUser);
 authRouter.post("/login", auth.loginUser);
 authRouter.post("/refresh", auth.refreshAccessToken);
 authRouter.post("/logout", auth.logoutUser);
+authRouter.post("/forgot-password", auth.forgotPassword);
 authRouter.post("/verify-reset-code", auth.verifyResetCode);
 authRouter.post("/reset-password", auth.resetPassword);
 authRouter.post("/forgot-username", auth.forgotUsername);
+authRouter.post(
+  "/toggle-profile-visibility",
+  verifyAccessToken,
+  auth.toggleProfileVisibility
+);
 
 export default authRouter;
